93deead2e3011609cb45c27145db5952d8f884bf
最佳人选 <wcj343169893@163.com>
2021-04-01 18:04:53 +0800

    Update self.js
    建议换一下位置，避免data里面存在uid参数

--- uniCloud-aliyun/cloudfunctions/uni-admin/controller/self.js
+++ uniCloud-aliyun/cloudfunctions/uni-admin/controller/self.js
@@ -6,8 +6,8 @@ module.exports = class UserController extends Controller {
     async changePwd() {
         const data = this.ctx.data
         return uniID.updatePwd({
-            uid: this.ctx.auth.uid,
-            ...data
+            ...data,
+            uid: this.ctx.auth.uid
         })
     }
 }
